28cabf32f38140a65b68bfb426a1795908556bc2
Andor Salamon <salamon.andor@gmail.com>
2017-10-28 11:50:03 +0200

    minisocialnetwork
    Version 1.1

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use App\Article;
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\DB;
 
 class ArticlesController extends Controller
 {
     public function index()
     {
-        //
+        $articles = Article::paginate(10);
+
+        return view('articles.index', compact('articles'));
     }
 
     public function create()
@@ -20,46 +23,43 @@ class ArticlesController extends Controller
 
     public function store(Request $request)
     {
-        /*
-        $article = new Article;
-
-        $article->user_id = Auth::user()->id;
-        $article->content = $request->content;
-        $article->live = (boolean)$request->live;
-        $article->post_on = $request->post_on;
-
-        $article->save();
-        */
-
         Article::create($request->all());
 
-        /*
-        Article::create([
-            'user_id' => Auth::user()->id,
-            'content' => $request->content,
-            'live' => $request->live,
-            'post_on' => $request->post_on
-        ]);
-        */
+        return redirect('/articles');
     }
 
     public function show($id)
     {
-        //
+        $article = Article::findOrFail($id);
+
+        return view('articles.show', compact('article'));
     }
 
     public function edit($id)
     {
-        //
+        $article = Article::findOrFail($id);
+
+        return view('articles.edit', compact('article'));
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $article = Article::findOrFail($id);
+
+        if(!isset($request->live))
+            $article->update(array_merge($request->all(), ['live' => false]));
+        else
+            $article->update($request->all());
+
+        return redirect('/articles');
     }
 
     public function destroy($id)
     {
-        //
+        $article = Article::findOrFail($id);
+
+        $article->forceDelete();
+
+        return redirect('/articles');
     }
 }
